9b841618dd8342dd5f355f82b4b72f2b0a56280a
apriasmoro <apriasmoro@gmail.com>
2022-08-23 23:08:53 +0700

    Update direct.js

--- direct.js
+++ direct.js
@@ -1,12 +1,10 @@
     //<![CDATA[
-    $(document).ready(function()
-    {
-    $('body').addClass('xepo_ads');
+    $(document).ready(function(){
+        $('body').addClass('xepo_ads');
     });
 
-    $(document).on('click','.xepo_ads',function(e)
-    {
-    $(this).removeClass('xepo_ads');
-    window.open('https://www.highperformancegate.com/k7td73ukb?key=96a7d7c160b18c4408d83ae67640311d', '_blank');
+    $(document).on('click','.xepo_ads',function(e){
+        $(this).removeClass('xepo_ads');
+        window.open('https://www.highperformancegate.com/k7td73ukb?key=96a7d7c160b18c4408d83ae67640311d', '_blank');
     });
     //]]>
